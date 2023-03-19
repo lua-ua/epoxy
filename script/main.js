593021288973f1c95e6dc96595880525326bff2b
lua-ua <121397690+lua-ua@users.noreply.github.com>
2023-03-20 00:28:59 +0200

    Delete script directory

--- script/main.js
+++
@@ -1,7 +0,0 @@
-/*let searchIconScale = document.getElementById('search-start');
-let searchVisible = document.getElementById('menu-user__search-form-in-computer');
-
-searchIconScale.addEventListener('click', function() {
-    searchIconScale.classList.toggle('scale');
-    searchVisible.classList.toggle('visible');
-});*/
