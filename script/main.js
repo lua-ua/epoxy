31df3e7f414efdaef7e39f7e988ad7845d99887e
lua-ua <121397690+lua-ua@users.noreply.github.com>
2023-03-20 00:30:39 +0200

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,7 @@
+/*let searchIconScale = document.getElementById('search-start');
+let searchVisible = document.getElementById('menu-user__search-form-in-computer');
+
+searchIconScale.addEventListener('click', function() {
+    searchIconScale.classList.toggle('scale');
+    searchVisible.classList.toggle('visible');
+});*/
